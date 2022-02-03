515f0e500e34e4cc18e753b64ed000dd7b15f2ad
Zhengchun Liu <lzhengchun@users.noreply.github.com>
2022-02-03 14:04:54 -0600

    Update progMIP.py

--- progMIP.py
+++ progMIP.py
@@ -74,7 +74,7 @@ def re_allocate(cmap, jmin, jmax, Ns, Os, Tfwd, res_up, res_dw, time_limit, solv
 
         m.add_sos([(w4sappro[_j][_i], Ns[_j][_i]) for _i in I], 2)  
 
-        S_approx[_j] = xsum(Ps[_j][_i] * w4sappro[_j][_i] for _i in I)
+        S_approx[_j] = xsum(Os[_j][_i] * w4sappro[_j][_i] for _i in I)
 
     # scale up/down cost
     dummy4upcost = [m.add_var(var_type=BINARY) for _j in J]
